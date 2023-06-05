1f94e8705048a667c4b735d5806c3b406479cc9f
Noah S <Noah3141@gmail.com>
2023-06-04 21:58:41 -0400

    Changed basi lint rules (and no misused promises)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,33 +3,43 @@ const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+    overrides: [
+        {
+            extends: [
+                "plugin:@typescript-eslint/recommended-requiring-type-checking",
+            ],
+            files: ["*.ts", "*.tsx"],
+            parserOptions: {
+                project: path.join(__dirname, "tsconfig.json"),
+            },
+        },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+    },
+    plugins: ["@typescript-eslint"],
+    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    rules: {
+        "react/no-unescaped-entities": ["off"],
+        "@typescript-eslint/no-misused-promises": [
+            "error",
+            {
+                checksVoidReturn: false,
+            },
+        ],
+        "@typescript-eslint/consistent-type-imports": [
+            "warn",
+            {
+                prefer: "type-imports",
+                fixStyle: "inline-type-imports",
+            },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { argsIgnorePattern: "^_" },
+        ],
+    },
 };
 
 module.exports = config;
