95881bd2adb48b1854c48fb6e952c8d958d8e382
Noah S <Noah3141@gmail.com>
2023-06-04 21:59:07 -0400

    Added font to tailwind as "main"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            main: ["Inter", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
 } satisfies Config;
